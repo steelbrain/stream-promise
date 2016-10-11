f2b2f10feffc97770e2539deab99eb71c331926e
Steel Brain <i@steelbrain.me>
2016-10-11 07:00:44 +0530

    :art: Rewrite main function

---
+++ src/main.js
@@ -0,0 +1,29 @@
+/* @flow */
+
+export default function createStreamPromise(stream: Object, bytesLimit: ?number): Promise<string> {
+  return new Promise(function(resolve, reject) {
+    let data = []
+    let dataLength = 0
+
+    stream.on('data', function(chunk) {
+      if (!Array.isArray(data)) {
+        // Stream harvesting closed
+        return
+      }
+      dataLength += chunk.length
+      if (dataLength > bytesLimit) {
+        data = null
+        reject(new Error('Stream body too big'))
+      }
+      data.push(chunk)
+    })
+
+    stream.on('error', function(error) {
+      data = null
+      reject(error)
+    })
+    stream.on('end', function() {
+      resolve(data.join(''))
+    })
+  })
+}
